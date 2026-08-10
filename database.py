689de9bd35534e1ed152b1168c4a5befb18d8c9c
ismailibnesyed <iammdismailhossain.com>
2026-08-10 11:38:42 +0600

    postgresql connection added

--- database.py
+++ database.py
@@ -4,8 +4,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Database connection URL for PostgresSQL
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.gkelvtwveftrpmocmyaf:[ismail_180162192]@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
-# ismail_180162192
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.gkelvtwveftrpmocmyaf:[ismail180162192]@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
+
+# ismail180162192
 
 # Database connection URL for SQLite
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'  # SQLite database URL
