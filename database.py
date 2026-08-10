e0f7caa311568b63c8d058d7ea4123c3d00bde0a
ismailibnesyed <iammdismailhossain.com>
2026-08-10 11:00:19 +0600

    postggresql connection added

--- database.py
+++ database.py
@@ -4,15 +4,15 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Database connection URL for PostgresSQL
-DATABASE_URL = 'postgresql://postgres.gkelvtwveftrpmocmyaf:[ismail_180162192]@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.gkelvtwveftrpmocmyaf:[ismail_180162192]@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 # ismail_180162192
 
 # Database connection URL for SQLite
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'  # SQLite database URL
 
 # Create the engine that talks to the database
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
-engine = create_engine(DATABASE_URL)
+# engine = create_engine(SQLALCHEMY_DATABASE_URL) # sql lite er jonno
+engine = create_engine(SQLALCHEMY_DATABASE_URL) # postgreSQL ar jonno
 
 # Create session factory for database requests
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
